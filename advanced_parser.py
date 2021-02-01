627fe2b99086d45ddcfb38f3ba2738f5487ad242
Abhinav Joshi <43189541+abhijoshi2000@users.noreply.github.com>
2021-01-31 22:14:38 -0500

    Update advanced_parser.py

--- advanced_parser.py
+++ advanced_parser.py
@@ -6,6 +6,6 @@ from bs4 import BeautifulSoup
 # Advanced Stats Parser
 URL = 'https://www.basketball-reference.com/players/e/embiijo01/gamelog-advanced/2021/'
 soup = BeautifulSoup(urlopen(URL), "html.parser")
-for elem in soup.findAll("tr", {"id":"pgl_advanced"}):
+for elem in soup.findAll("table", {"id":"pgl_advanced"}):
     print(str(elem))
 data = []
